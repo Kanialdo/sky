c307f64293fde0aa2a79e17dd50e359c45f9df34
Krystian Kaniowski <kanialdo@gmail.com>
2022-05-26 22:48:35 +0200

    Add moon module

---
+++ app/src/main/java/pl/krystiankaniowski/sky/SkyApplicationContent.kt
@@ -0,0 +1,26 @@
+package pl.krystiankaniowski.sky
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import pl.krystiankaniowski.sky.moon.MoonScreen
+import pl.krystiankaniowski.sky.ui.theme.SkyTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SkyApplicationContent() {
+    SkyTheme {
+        Scaffold(
+            topBar = { SmallTopAppBar(title = { Text("Sky") }) }
+        ) {
+            Box(modifier = Modifier.padding(it)) {
+                MoonScreen()
+            }
+        }
+    }
+}
