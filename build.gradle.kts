790a054e8b8bf0935bafc72f7384ae18442b49d0
Krystian Kaniowski <kanialdo@gmail.com>
2022-06-05 22:57:46 +0200

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 buildscript {
     repositories {
         google()
@@ -13,6 +15,38 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.detekt)
+}
+
+allprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt {
+        ignoreFailures = false
+        buildUponDefaultConfig = true
+        parallel = true
+        autoCorrect = false
+        config.setFrom(files(project.rootDir.resolve("detekt.yml")))
+
+        dependencies {
+            detektPlugins(rootProject.libs.detekt.plugins.formatting)
+        }
+    }
+
+    tasks {
+        withType<Detekt> {
+            this.jvmTarget = "1.8"
+            reports {
+                html.required.set(true)
+            }
+        }
+    }
 }
 
 tasks.register("clean", Delete::class) {
--- libs/compose/src/main/java/pl/krystiankaniowski/sky/compose/SkyColor.kt
+++ libs/compose/src/main/java/pl/krystiankaniowski/sky/compose/SkyColor.kt
@@ -2,5 +2,5 @@ package pl.krystiankaniowski.sky.compose
 
 import androidx.compose.ui.graphics.Color
 
-val Blue70 = Color(0xFF1976D2)
-val Blue90 = Color(0xFF0D47A1)
+val Blue70 = @Suppress("MagicNumber") Color(0xFF1976D2)
+val Blue90 = @Suppress("MagicNumber") Color(0xFF0D47A1)
